96866d6f54f9abcd4d67dd2e788c303240e55e1c
Jonathan Dutra Hartmann <120300587+J00naTHan@users.noreply.github.com>
2024-05-01 05:32:40 -0300

    Update grafo.py

--- grafo.py
+++ grafo.py
@@ -19,7 +19,7 @@ class Graph:
       except KeyError:
         raise Except('O vértice passado não consta no grafo')
       try:
-          return bool(vertex_1['edges'][vertex_2])
+          return bool(self.nodes[vertex_1]['edges'][vertex_2])
       except KeyError:
           return False
     raise Exception('Os vértices precisam ser passados como ID\'s do tipo int')
@@ -50,55 +50,21 @@ class Graph:
       raise Exception('Os vértices precisam ser passados como ID\'s do tipo int')
 
 
-class Aresta:
-  def __init__(self, nodo1, nodo2, custo):
-    if isinstance(custo, float) and custo >= 0:
-      self.custo = custo
-    else:
-      raise Exception('O custo para essa aresta deve ser do tipo int (inteiro) e deve ser maior ou igual a -1')
-    if isinstance(nodo1, Nodo) and isinstance(nodo2, Nodo):
-      self.nodo1 = nodo1
-      self.nodo2 = nodo2
-    else:
-      raise Exception('Nodos devem ser objetos do tipo Nodo')
-
-
-class Nodo:
-  def __init__(self, id, latitude, longitude):
-    if isinstance(id, int) and isinstance(longitude, float) and isinstance(latitude, float):
-      self.id = id
-      self.lat = latitude
-      self.lon = longitude
-      self.arestas = {}
-    else:
-      raise Exception('Um nodo requer 1 valor do tipo int (inteiro) para id, e 2 valores do tipo float para latitude e longitude')
-
-  def __eq__(self, nodo):
-    if isinstance(nodo, Nodo):
-      return self.id == nodo.id
-    elif isinstance(nodo, int):
-      return self.id == nodo
-    raise ValueError('A comparação deve ser feita com outro objeto do tipo Nodo ou do tipo int (inteiro)')
-
-  def __hash__(self):
-    return hash(self.id)
-
-# erro de duplicata de dicionário
 def read_graph(filename: str):
-    """Lê uma estrutura de grafo de um arquivo e retorna a estrutura"""
-    with open(filename, "rt") as input_file:
-        vertex_count, nodes = int(input_file.readline().strip()), {}
-        for _ in range(vertex_count):
-            index, latitude, longitude = input_file.readline().strip().split()
-            nodes[index] = {'lat': latitude, 'lon': longitude, 'edges': {}}
-            
-        edge_count = int(input_file.readline().strip())
-        for _ in range(edge_count):
-            from_vertex, to_vertex, cost = input_file.readline().strip().split()
-            try:
-              nodes[from_vertex]['edges'][to_vertex] = cost
-              nodes[to_vertex]['edges'][from_vertex] = cost
-            except KeyError:
-              raise Exception('You are attempting to acess a node that doesn\'t exist')
-        graph = Graph(vertex_count, edge_count, nodes)
-    return graph
+  """Lê uma estrutura de grafo de um arquivo e retorna a estrutura"""
+  with open(filename, "rt") as input_file:
+    vertex_count, nodes = int(input_file.readline().strip()), {}
+    for _ in range(vertex_count):
+      index, latitude, longitude = input_file.readline().strip().split()
+      nodes[index] = {'lat': latitude, 'lon': longitude, 'edges': {}}
+
+    edge_count = int(input_file.readline().strip())
+    for _ in range(edge_count):
+        from_vertex, to_vertex, cost = input_file.readline().strip().split()
+        try:
+          nodes[from_vertex]['edges'][to_vertex] = cost
+          nodes[to_vertex]['edges'][from_vertex] = cost
+        except KeyError:
+          raise Exception('You are attempting to acess a node that doesn\'t exist')
+    graph = Graph(vertex_count, edge_count, nodes)
+  return graph
